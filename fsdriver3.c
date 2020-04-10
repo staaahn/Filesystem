5250359d7a4f4025a19008565b537dbac818d1f3
Robin <robert.ian.clarkson@gmail.com>
2020-04-10 14:39:21 -0700

    sudo code

--- fsdriver3.c
+++ fsdriver3.c
@@ -1,1 +1,40 @@
 // Driver File
+
+createFile() {
+  LBA_write( stuff) 
+}
+
+readFile() {
+ LBA_read 
+}
+
+listDir() {
+  LBA_read()
+}
+
+createDir() {
+  LBA_write()
+}
+
+#include "CreateFile.h"
+#include "ListDir.h"
+#include "CreateDir.h"
+
+
+main () {
+  startpartition (stuff) // beer
+  while(true) {
+    //menu
+    1. create a dir
+      createDir(dir name);
+    2. list dir
+      listDir();
+    3. create a file
+    4. rm a file
+            enter the filename
+            removed file (filename)
+    10. enter q to quit
+  }
+    
+  closepartition (stuff) // beer
+}
