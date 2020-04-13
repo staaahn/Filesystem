54e3833a15beeeca4b563bbbdcbd11948b4ce7b5
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-04-13 12:50:04 -0700

    Tokenized

--- fsdriver3.c
+++ fsdriver3.c
@@ -4,12 +4,21 @@
 #include <string.h>
 #include <math.h>
 
-#include "fsLow.h"
-#include "tokenize.c"
+#include "fsLow.h" /* (startPartitionSystem) (closePartitionSystem) (LBAwrite) (LBAread) */
+#include "tokenize.c" /* (tokenize) (print) */
 
+#define LINE_LENGTH 256 // CHAR
+#define MAX_ARGS 8
+#define ARG_LENGTH 32 // CHAR
+
+// struct arguments {
+//     char arguments[MAX_ARGS][ARG_LENGTH];
+//     char* opt;
+//     int arg_count;
+// };
 
 int main (int argc, char *argv[]) {
-    char * filename;
+    char* filename;
 	uint64_t volumeSize;
 	uint64_t blockSize;
     int retVal;
@@ -17,9 +26,9 @@ int main (int argc, char *argv[]) {
 
     /* Check for correct argc */
 	if (argc == 4){
-		filename = argv[1];
-		volumeSize = atoll (argv[2]);
-		blockSize = atoll (argv[3]);
+		filename     = argv[1];
+		volumeSize   = atoll (argv[2]); // must be greater than 500,000 and a power of 2
+		blockSize    = atoll (argv[3]); // must be greater than 512 and a power of 2
 	} else {
         printf("Need 3 args (filename volumeSize blcokSize\n");
         return EXIT_FAILURE;
@@ -33,45 +42,51 @@ int main (int argc, char *argv[]) {
         printf("\tresult: success\n");
     } else if(retVal == -1) {
         printf("\tresult: file exists but can not open for write\n");
+        // return EXIT_FAILURE;
     } else if(retVal == -2) {
         printf("\tresult: insufficient space for the volume\n");
+        // return EXIT_FAILURE;
     } else {
         printf("\tresult: %d\n", retVal);
+        // return EXIT_FAILURE;
     }
 
     /* MENU */
-    char* opt = malloc(sizeof(char) * 8); // 8 letter word
-    char* line = malloc(sizeof(char) * 255); // 255 letter word
-    char** arguments;
+    char* opt = malloc(sizeof(char) * ARG_LENGTH);
+    char line[LINE_LENGTH];
+    char arguments[MAX_ARGS][ARG_LENGTH];
     int arg_count = 0;
-    while(strcmp(opt, "exit") != 0) {
-        printf("NOTE: type \"exit\" to exit this prompt\n"); // prompt
+
+    printf("\nNOTE: type \"exit\" to exit this prompt\n"); // prompt
+    // while(strcmp(opt, "exit") != 0) {
         printf("$ "); // prompt
-        scanf("%s", line);
-        tokenize(line, arguments, arg_count);
+        fgets(line, LINE_LENGTH, stdin);
+        tokenize(line, &arguments, &arg_count);
+        print(arguments, arg_count);
         opt = arguments[0];
-        if(strcmp(opt, "ls") == 0) {
-            listDir(arguments, arg_count);
-        } else if(strcmp(opt, "mkdir") == 0) {
-            createDir(arguments, arg_count);
-        } else if(strcmp(opt, "touch") == 0) {
-            createFile(arguments, arg_count);
-        } else if(strcmp(opt, "rm") == 0) {
-            removeFile(arguments, arg_count);
-        } else if(strcmp(opt, "cp") == 0) {
-            copyFile(arguments, arg_count);
-        } else if(strcmp(opt, "mv") == 0) {
-            moveFile(arguments, arg_count);
-        } else if(strcmp(opt, "set") == 0) {
-            setMetaData(arguments, arg_count);
-        } else if(strcmp(opt, "special1") == 0) {
-            special1(arguments, arg_count);
-        } else if(strcmp(opt, "special2") == 0) {
-            special2(arguments, arg_count);
-        }
-    }
+        // if(strcmp(opt, "ls") == 0) {
+        //     listDir(arguments, arg_count);
+        // } else if(strcmp(opt, "mkdir") == 0) {
+        //     createDir(arguments, arg_count);
+        // } else if(strcmp(opt, "touch") == 0) {
+        //     createFile(arguments, arg_count);
+        // } else if(strcmp(opt, "rm") == 0) {
+        //     removeFile(arguments, arg_count);
+        // } else if(strcmp(opt, "cp") == 0) {
+        //     copyFile(arguments, arg_count);
+        // } else if(strcmp(opt, "mv") == 0) {
+        //     moveFile(arguments, arg_count);
+        // } else if(strcmp(opt, "set") == 0) {
+        //     setMetaData(arguments, arg_count);
+        // } else if(strcmp(opt, "special1") == 0) {
+        //     special1(arguments, arg_count);
+        // } else if(strcmp(opt, "special2") == 0) {
+        //     special2(arguments, arg_count);
+        // }
+    // }
+    // free(opt);
 
     /* Close Partition */
-    closePartitionSystem();
+    // closePartitionSystem();
     return EXIT_SUCCESS;
 }
--- tokenize.c
+++ tokenize.c
@@ -1,3 +1,26 @@
-void tokenize(char** matrix, char*line, int arg_count) {
+#define LINE_LENGTH 256 // CHAR
+#define MAX_ARGS 8
+#define ARG_LENGTH 32 // CHAR
+#define DELIMITERS " "
 
+void print(char args[MAX_ARGS][ARG_LENGTH], int argc) {
+    for(int i = 0; i < argc; i++) {
+        printf("%d : %s\n", i, args[i]);
+    }
 }
+
+void tokenize(char line[LINE_LENGTH], char args[MAX_ARGS][ARG_LENGTH], int* argc) {
+    char* token;
+    char* rest = line;
+    *argc = 0;
+    while ((token = strtok_r(rest, DELIMITERS, &rest))) {
+        if(*argc > MAX_ARGS) {
+            printf("****** TO MANY ARGS ******\n");
+            break;
+        }
+        strcpy(args[*argc], token);
+        *argc += 1;
+    }
+}
+
+
